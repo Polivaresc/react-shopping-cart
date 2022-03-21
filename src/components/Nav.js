cba3d07d5b6be169ffdfc27656ae278daabeece3
Polivaresc <paulaolca98@gmail.com>
2022-03-21 20:29:08 +0100

    adding logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faStore } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
     return (
         <nav className="nav-bar">
-            <h1><Link to="/" className="logo">LOGO</Link></h1>
+            <Link to="/" className="logo"><img src="./plantLogo.svg" alt="plant logo"/><span>Plantix</span></Link>
             <ul className="nav-links">
-                <li><Link to="/" className="link">Home</Link></li>
-                <li><Link to="shop" className="link">Shop</Link></li>
+                <li><Link to="/" className="link"><span>Home</span><FontAwesomeIcon icon={faHome}/></Link></li>
+                <li><Link to="shop" className="link"><span>Shop</span><FontAwesomeIcon icon={faStore}/></Link></li>
             </ul>
         </nav>
     )
--- src/components/Shoppage.js
+++ src/components/Shoppage.js
@@ -1,13 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
 const Shoppage = () => {
     return ( 
         <div className="shop-page">
             <div className="sticky-bar">
                 <span>Items in cart : 0</span>
-                <button>Go to cart</button>
+                <button className="cart-button">Go to cart<FontAwesomeIcon icon={faCartShopping} /></button>
             </div>
             <div>Shop Items</div>
         </div>
     )
 }
+
+/* const Item = () => {
+    return (
+        <div>
+            <img src={item.image}/>
+        </div>
+    )
+} */
 
 export default Shoppage;
